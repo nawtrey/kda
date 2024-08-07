4ad223c11e56bdeee2c3654b01b1837f2607eb82
Nikolaus C. Awtrey <nawtrey@asu.edu>
2024-08-07 08:09:16 -0700

    RTD testing

--- docs/conf.py
+++ docs/conf.py
@@ -13,9 +13,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 # Incase the project was not installed
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 import kda
 
